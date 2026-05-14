57e4b81e23094001b6d417cdf40befa438bdc6f5
qwe11lk <122397402+122-547@users.noreply.github.com>
2026-05-14 12:40:22 +0300

    fixed

--- admin.py
+++ admin.py
@@ -1,27 +1,27 @@
 import socket
-import threading
 import cv2
 import numpy as np
 
-def send_admin_message():
-    global tcp_client
-    try:
-        tcp_client.conenct(("147.45.79.209", 5824))
-        tcp_client.send(b"<ADMIN>")
-        tcp_client.close()
-    finally:
-        if tcp_client:
-            tcp_client.close()
+# def send_admin_message():
+#     global tcp_client
+#     try:
+#         tcp_client.connect(("147.45.79.209", 5824))
+#         tcp_client.send(b"<ADMIN>")
+#         tcp_client.close()
+#     finally:
+#         if tcp_client:
+#             tcp_client.close()
 
 udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 window_name = "Cam"
-
+# send_admin_message()
 try:
     cv2.namedWindow(window_name)
     while True:
         data, addr = udp_client.recvfrom(65536)
+        
         if not data:
+            udp_client.sendto(b"<ADMIN>", ("147.45.79.209", 5823))
             continue
 
         np_arr = np.frombuffer(data, dtype=np.uint8)
--- server.py
+++ server.py
@@ -2,28 +2,27 @@ import socket
 import threading
 
 
-def get_admin_addr():
-    global admin, server_tcp
-    try:
-        server_tcp.bind(("localhost", 5824))
-        print("TCP сервер запущен и ожидает подключение админа...")
-        server_tcp.accept(1)
-        conn, addr = server_tcp.accept()
-        while True:
-            data = conn.recv(1024)
-            if data == b"<ADMIN>":
-                print(f"Admin ({addr}) подключился к серверу.")
-                admin = addr
-                break
+# def get_admin_addr():
+#     global admin, server_tcp
+#     try:
+#         server_tcp.bind(("localhost", 5824))
+#         print("TCP сервер запущен и ожидает подключение админа...")
+#         server_tcp.accept(1)
+#         conn, addr = server_tcp.accept()
+#         while True:
+#             data = conn.recv(1024)
+#             if data == b"<ADMIN>":
+#                 print(f"Admin ({addr}) подключился к серверу.")
+#                 admin = addr
+#                 break
             
-    finally:
-        server_tcp.close()
+#     finally:
+#         server_tcp.close()
     
 
-server = socket.socket(socket.AF_INET, socket.SOCK_GDRAM)
-server_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 admin = None
-waiting_admin = threading.Thread(target=get_admin_addr, daemon=True)
+# waiting_admin = threading.Thread(target=get_admin_addr, daemon=True)
 try:
     server.bind(("localhost", 5823))
     print("UDP Сервер запущен и ожидает кадры...")
@@ -35,8 +34,8 @@ try:
             server.sendto(data, admin)
             print(f"Кадры пересланы админу ({admin}).")
         else:
-            print("Админ отключился.")
-            continue
+            if data == b"<ADMIN>":
+                admin = addr
 
 finally:
     server.close()
